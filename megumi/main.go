fa19ea9d4adf3164a27449ad1b1993dd8a705604
Sudharsan-Deloitte-DBG <160199554+Sudharsan-Deloitte-DBG@users.noreply.github.com>
2024-05-12 19:00:40 +0530

    megumi tries black flash

--- megumi/main.go
+++ megumi/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Sudharsan-Deloitte-DBG/yuji/megumi"
+)
 
 func main() {
 	fmt.Println("This package belongs to Megumi.")
+	megumi.MegumiChainsBlackFlash()
 }
